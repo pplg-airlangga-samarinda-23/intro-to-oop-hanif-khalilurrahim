baed08ed51a031059019b4d75601dc5729526291
hanif.khalilurrahim3@gmail.com <hanif.khalilurrahim3@gmail.com>
2024-10-02 15:42:37 +0800

    phase 2

--- soal-3/Mobil.php
+++ soal-3/Mobil.php
@@ -1,27 +1,40 @@
 <?php
 class Mobil {
     // lengkapi dengan atribut dan metode sesuai pada buku
-
+    public $melaju;
+    public $merek;
+    public $belok;
+    public $bensin;
     // contoh metode melaju()
-    public function melaju() {
-        echo "Mobil sedang melaju.";
-    }
     
     public function melaju() {
-        echo "Mobil sedang melaju.";
+        echo "Mobil sedang melaju dengan kecepatan penuh ";
+    }
+    
+    public function merek() {
+        echo "Merek mobil yaitu bugatti warna hitam ";
     }
 
-    public function melaju() {
-        echo "Mobil sedang melaju.";
+    public function belok() {
+        echo "Mobil sedang jamping jamping ";
     }
 
-    public function melaju() {
-        echo "Mobil sedang melaju.";
+    public function bensin() {
+        echo "Bensin mobil penuh F ";
     }
 }
 
 // buat sebuah objek mobil dan lengkapi atributnya
-
+$mobilSaya = new Mobil("Toyota", "Hitam", 80);
 
 // panggil keempat metode yang terdapat pada kelas Mobil
+$melaju -> melaju();
+$merek -> merek();
+$belok -> belok();
+$bensin -> bensin();
+echo $melaju;
+echo $merek;
+echo $belok;
+echo $bensin;
+
 ?>
