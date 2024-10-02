6ad4ae65c83e660c9e8eb5640ee57e02be362c1c
hanif.khalilurrahim3@gmail.com <hanif.khalilurrahim3@gmail.com>
2024-10-02 14:21:50 +0800

    phase 1

--- soal-1/Kotak.php
+++ soal-1/Kotak.php
@@ -2,28 +2,40 @@
 class PersegiPanjang
 {
     // deklarasikan panjang dan lebar
-
+    public $panjang;
+    public $lebar;
 
     // deklarasikan metode konstruktor
-
+    public function _construct($panjang, $lebar)
+    {
+        $this -> panjang = $panjang;
+        $this -> lebar = $lebar;
+    }
 
     // deklarasikan metode mutator dan asesor
+    public function __get($atribut) {
+        return $this -> $atribut;
+    }
 
-
+    public function _set($atribut, $nilai) {
+        return $atribut;
+    }
 
     public function hitungLuas()
     {
         // lengkapi kode untuk menghitung luas persegi panjang
-
+        return $this->panjang * $this->lebar;
     }
 
     public function hitungKeliling()
     {
         // lengkapi dengan kode untuk menghitung keliling persegi panjang
-
+        return 2 * ($this->panjang + $this->lebar);
     }
 }
-
 $contoh = new PersegiPanjang(12, 20);
+$contoh -> panjang = 12;
+$contoh -> lebar = 20;
 echo "Luas Persegi Panjang: " . $contoh->hitungLuas() . "<br>";
 echo "Keliling Persegi Panjang: " . $contoh->hitungKeliling() . "<br>";
+?>
--- soal-2/Waktu.php
+++ soal-2/Waktu.php
@@ -2,9 +2,14 @@
 class Waktu
 {
     // deklarasikan atribut sesuai ditentukan buku
-
+    public $jam;
+    public $menit;
+    public $detik;
     // deklarasikan metode sesuai yang diminta buku
 }
 
 $contoh = new Waktu(15,34,50);
-echo "Sekarang pukul".$contoh->jam.":".$contoh->menit.":".$contoh->detik."<br>"; //output: Sekarang pukul 15:34:50
+$contoh -> jam = 15;
+$contoh -> menit = 34;
+$contoh -> detik = 50;
+echo "Sekarang pukul ".$contoh->jam.":".$contoh->menit.":".$contoh->detik."<br>"; //output: Sekarang pukul 15:34:50
